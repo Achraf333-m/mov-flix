3d97b1665ee22205e862519c815d3ec5852e2ac7
Achraf333-m <achrafdaimallah2003@gmail.com>
2023-11-18 00:38:24 -0500

    getserversideprops fix

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,4 +1,10 @@
-function Banner() {
+import { Movie } from "@/typings";
+
+interface props {
+    TopRated: Movie[]
+}
+
+function Banner({ TopRated }:props) {
     return (
         <div>
             Enter
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,39 @@
 import Banner from "@/components/Banner";
 import Header from "@/components/Header";
+import { Movie } from "@/typings";
 import links from "@/utils/links";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 
-function Home() {
+interface props {
+  Trending: Movie[];
+  Originals: Movie[];
+  TopRated: Movie[];
+  Action: Movie[];
+  Comedy: Movie[];
+  Horror: Movie[];
+  Romance: Movie[];
+  Documentaries: Movie[];
+}
+
+function Home({
+  Trending,
+  Originals,
+  TopRated,
+  Action,
+  Comedy,
+  Horror,
+  Romance,
+  Documentaries,
+}: props) {
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
       <div className="px-10 py-4">
-        <Header/>
-        <Banner />
+        <Header />
+        <Banner TopRated={TopRated}/>
       </div>
     </>
   );
@@ -19,7 +41,7 @@ function Home() {
 
 export default Home;
 
-export const getServerSideProps = async() => {
+export const getServerSideProps:GetServerSideProps = async () => {
   const [
     Trending,
     Originals,
@@ -28,28 +50,28 @@ export const getServerSideProps = async() => {
     Comedy,
     Horror,
     Romance,
-    Documentaries
+    Documentaries,
   ] = await Promise.all([
-    fetch(links.Trending).then(res => res.json()),
-    fetch(links.Originals).then(res => res.json()),
-    fetch(links.TopRated).then(res => res.json()),
-    fetch(links.Action).then(res => res.json()),
-    fetch(links.Comedy).then(res => res.json()),
-    fetch(links.Horror).then(res => res.json()),
-    fetch(links.Romance).then(res => res.json()),
-    fetch(links.Documentaries).then(res => res.json())
-  ])
+    fetch(links.Trending).then((res) => res?.json()),
+    fetch(links.Originals).then((res) => res?.json()),
+    fetch(links.TopRated).then((res) => res?.json()),
+    fetch(links.Action).then((res) => res?.json()),
+    fetch(links.Comedy).then((res) => res?.json()),
+    fetch(links.Horror).then((res) => res?.json()),
+    fetch(links.Romance).then((res) => res?.json()),
+    fetch(links.Documentaries).then((res) => res?.json()),
+  ]);
 
   return {
     props: {
-      Trending: Trending.results,
-      Originals: Originals.results,
-      TopRated: TopRated.results,
-      Action: Action.results,
-      Comedy: Comedy.results,
-      Horror: Horror.results,
-      Romance: Romance.results,
-      Documentaries: Documentaries.results,
-    }
-  }
-}
+      Trending: Trending.results || null,
+      Originals: Originals.results || null,
+      TopRated: TopRated.results || null,
+      Action: Action.results || null,
+      Comedy: Comedy.results || null,
+      Horror: Horror.results || null,
+      Romance: Romance.results || null,
+      Documentaries: Documentaries.results || null,
+    },
+  };
+};
