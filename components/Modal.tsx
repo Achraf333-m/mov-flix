873dcca0205d8957d65b226ef3caa216ccaf2cf2
Achraf333-m <achrafdaimallah2003@gmail.com>
2023-12-01 02:07:10 -0500

    trailer/react player init

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -11,8 +11,8 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 300,
-  bgcolor: 'background.paper',
+  width: 400,
+  bgcolor: 'black',
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
@@ -30,7 +30,6 @@ export default function BasicModal() {
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
-        className=''
         
       >
         <Box sx={style}>
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,6 +1,7 @@
 import { modalState } from "@/atoms/atom";
 import { imgUrl } from "@/constants/url";
 import { Movie } from "@/typings";
+import { trailer } from "@/utils/trailer";
 import { useRecoilState } from 'recoil'
 
 interface props {
@@ -9,9 +10,12 @@ interface props {
 
 function Thumbnail({ Movie }:props) {
     const [showTrailer, setShowTrailer] = useRecoilState(modalState)
-    console.log(Movie.genre_ids)
+    const show_trailer = async() => {
+        const data = await trailer(Movie.id)
+        console.log(data)
+    }
     return (
-        <figure onClick={() => setShowTrailer(true)} className="h-40 w-72 flex-shrink-0 hover:scale-[102%] cursor-pointer">
+        <figure onClick={() => {setShowTrailer(true), show_trailer()} } className="h-40 w-72 flex-shrink-0 hover:scale-[102%] cursor-pointer">
             <img src={`${imgUrl}${Movie.backdrop_path || Movie.poster_path}`} alt="Thumbnail" className="relative rounded-md object-cover w-full h-full" />
         </figure>
     );
